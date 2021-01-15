27ccec4f66bb87982bccd564b1ef8f4b3f923989
Asish kumar ghosh <ruet.asish@gmail.com>
2021-01-15 20:47:40 +0600

    CSS update

---
+++ app.js
@@ -0,0 +1,11 @@
+const userScore=0;
+const computerScore=0;
+
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
